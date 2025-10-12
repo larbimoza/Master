31444e061f50f69fa122bbd6263795128517decb
Badreddine Bidouli <black@Badreddines-MBP.attlocal.net>
2025-10-12 13:19:46 -0500

    all

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-a=2
-b=3
-c=a+b
-print("hello"*2)
+Monday = True
+Tuesday = True
+Wednesday = True
+Thursday = True
+Friday = False
+
+
+def checkgym(day):
+    if day:
+        print("You have gym today")
+    else:
+        print("rest day")
+
+checkgym(Friday)
+
